88b77e55141b5f90cec09b2ee547b55134f0a14a
mikebenvenuti <benvenuti@sbcglobal.net>
2016-12-18 21:00:55 -0600

    Added some markup

--- dist/ch01_showStudent.js
+++ dist/ch01_showStudent.js
@@ -1,3 +1,18 @@
 /** ch01_showStudent.js on 12/18/2016. ...*/
 
+function showStudent(ssn) {
+
+    console.log(ssn);
+    var student = db.get(ssn);
+
+    if (student !== null) {
+        document.querySelector('#SSN').innerHTML =
+            '${student.firstname}',
+            '${student.lastname}';
+    }
+    else {
+        throw new Error ('Student not found!');
+    }
+}
+
 
--- readme.md
+++ readme.md
@@ -1,2 +1,19 @@
-Functional Programming in JavaScript
-Luis Atencio
+#Functional Programming in JavaScript
+
+##Luis Atencio
+
+
+**Map Reduce**
+
+**1. Map**
+
+	foreach in array perform the against the function
+	
+
+	
+	
+
+
+**2. Reduce**
+
+    Reduce array to number e.g.  Sum or Count
